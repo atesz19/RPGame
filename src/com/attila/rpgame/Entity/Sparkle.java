e743ac5b34c6b7ab566b5d8b2fc3dc2328ba8625
atesz19 <attika199912@gmail.com>
2022-04-07 15:03:35 +0200

    Update

---
+++ src/com/attila/rpgame/Entity/Sparkle.java
@@ -0,0 +1,35 @@
+// Simple class that plays animation
+// once and is removed.
+
+package com.attila.rpgame.Entity;
+
+import java.awt.Graphics2D;
+
+import com.attila.rpgame.Manager.Content;
+import com.attila.rpgame.TileMap.TileMap;
+
+public class Sparkle extends Entity {
+	
+	private boolean remove;
+	
+	public Sparkle(TileMap tm) {
+		super(tm);
+		animation.setFrames(Content.SPARKLE[0]);
+		animation.setDelay(5);
+		width = height = 16;
+	}
+	
+	public boolean shouldRemove() {
+		return remove;
+	}
+	
+	public void update() {
+		animation.update();
+		if(animation.hasPlayedOnce()) remove = true;
+	}
+	
+	public void draw(Graphics2D g) {
+		super.draw(g);
+	}
+	
+}
---
+++ src/com/attila/rpgame/Manager/GameStateManager.java
@@ -0,0 +1,83 @@
+// The GameStateManager does exactly what its
+// name says. It contains a list of GameStates.
+// It decides which GameState to update() and
+// draw() and handles switching between different
+// GameStates.
+
+package com.attila.rpgame.Manager;
+
+import java.awt.Graphics2D;
+
+import com.attila.rpgame.GameState.GameOverState;
+import com.attila.rpgame.GameState.GameState;
+import com.attila.rpgame.GameState.PauseState;
+import com.attila.rpgame.GameState.PlayState;
+
+
+public class GameStateManager {
+	
+	private boolean paused;
+	private PauseState pauseState;
+	
+	private GameState[] gameStates;
+	private int currentState;
+	private int previousState;
+	
+	public static final int NUM_STATES = 4;
+	public static final int PLAY = 2;
+	public static final int GAMEOVER = 3;
+	
+	public GameStateManager() {
+		
+		JukeBox.init();
+		
+		paused = false;
+		pauseState = new PauseState(this);
+		
+		gameStates = new GameState[NUM_STATES];
+		setState(PLAY);
+		
+	}
+	
+	public void setState(int i) {
+		previousState = currentState;
+		unloadState(previousState);
+		currentState = i;
+
+		if(i == PLAY) {
+			gameStates[i] = new PlayState(this);
+			gameStates[i].init();
+		}
+		else if(i == GAMEOVER) {
+			gameStates[i] = new GameOverState(this);
+			gameStates[i].init();
+		}
+	}
+	
+	public void unloadState(int i) {
+		gameStates[i] = null;
+	}
+	
+	public void setPaused(boolean b) {
+		paused = b;
+	}
+	
+	public void update() {
+		if(paused) {
+			pauseState.update();
+		}
+		else if(gameStates[currentState] != null) {
+			gameStates[currentState].update();
+		}
+	}
+	
+	public void draw(Graphics2D g) {
+		if(paused) {
+			pauseState.draw(g);
+		}
+		else if(gameStates[currentState] != null) {
+			gameStates[currentState].draw(g);
+		}
+	}
+	
+}
